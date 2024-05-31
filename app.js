18a46abbced02c32293dcb165147b77d8a41bb3b
Vicky-peace <victorpeace47@gmail.com>
2024-05-31 10:01:44 +0300

    looped through data successfully

--- app.js
+++ app.js
@@ -16,5 +16,41 @@ document.addEventListener('DOMContentLoaded', () =>{
     });
 
     //Display job listings
+    function displayJobs(jobs) {
+        jobListingContainer.innerHTML = '';
+        jobs.forEach(job =>{
+            const jobCard = document.createElement('div');
+            jobCard.classList.add('job-card');
+
+
+            const jobDetails = document.createElement('div');
+            jobDetails.classList.add('job-details');
+
+            //labels
+            let labels = '';
+            if(job.new) labels += `<span class ="label new">New!</span>`;
+            if(jobs.featured) labels +=  '<span class="label featured">Featured</span>';
+
+            //logo
+            const jobLogo = document.createElement('img');
+            jobLogo.src = job.logo;
+            jobLogo.alt = `${job.company} logo`;
+            jobLogo.classList.add('company-logo');
+            
+
+            jobDetails.innerHTML = `
+            <div>
+                ${labels}
+                <h2>${job.company}</h2>
+            </div>
+            <p>${job.position}</p>
+            <p>${job.postedAt} • ${job.contract} • ${job.location}</p>
+            <div class="job-tags">
+                ${job.languages.map(lang => `<span class="tag">${lang}</span>`).join('')}
+                ${job.tools.map(tool => `<span class="tag">${tool}</span>`).join('')}
+            </div>
+        `;
+        })
+    }
   
 })
